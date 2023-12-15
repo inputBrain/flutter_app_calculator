eff5e075b6095003e698e027f70ceb5a72739dd3
Alexander <vsany@meta.ua>
2023-12-15 19:46:36 +0200

    [+] In work: Package for ads: google_mobile_ads. Save settings state and on / off the ads according to the settings option

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -19,8 +24,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CalculatorScreen extends StatelessWidget {
+class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  bool _isAdEnabled = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAdSetting();
+  }
+
+  Future<void> _loadAdSetting() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _isAdEnabled = prefs.getBool('adsOn') ?? true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,22 +73,110 @@ class CalculatorScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.settings),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
+            onPressed: () async {
+              await Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => const SettingsScreen(),
               ));
+              _loadAdSetting(); // Reload ad setting after returning from settings
             },
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.75,
-          child: calc,
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: calc,
+            ),
+          ),
+          if (_isAdEnabled)
+            AdBannerWidget(
+              adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+              isAdEnabled: _isAdEnabled,
+            ),
+        ],
       ),
     );
+  }
+}
+
+class AdBannerWidget extends StatefulWidget {
+  final String adUnitId;
+  final bool isAdEnabled;
+
+  const AdBannerWidget({
+    Key? key,
+    required this.adUnitId,
+    required this.isAdEnabled,
+  }) : super(key: key);
+
+  @override
+  _AdBannerWidgetState createState() => _AdBannerWidgetState();
+}
+
+class _AdBannerWidgetState extends State<AdBannerWidget> {
+  BannerAd? _bannerAd;
+  bool _isAdLoaded = false;
+
+  void _createBannerAd() {
+    _bannerAd = BannerAd(
+      adUnitId: widget.adUnitId,
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          // Handle the error
+          ad.dispose();
+        },
+      ),
+    );
+
+    _bannerAd?.load();
+  }
+
+  @override
+  void didUpdateWidget(covariant AdBannerWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isAdEnabled != oldWidget.isAdEnabled) {
+      if (widget.isAdEnabled) {
+        _createBannerAd();
+      } else {
+        _bannerAd?.dispose();
+        _isAdLoaded = false;
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.isAdEnabled) {
+      _createBannerAd();
+    }
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isAdLoaded && widget.isAdEnabled
+        ? Container(
+      height: _bannerAd!.size.height.toDouble(),
+      width: _bannerAd!.size.width.toDouble(),
+      alignment: Alignment.center,
+      child: AdWidget(ad: _bannerAd!),
+    )
+        : const SizedBox();
   }
 }
 
@@ -104,7 +217,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
       body: ListView(
         children: <Widget>[
           SwitchListTile(
-            title: const Text('Ads'),
+            title: const Text('Adds'),
             subtitle: const Text('Turn this on or off to enable ads'),
             value: adsOn,
             activeColor: Colors.green,
