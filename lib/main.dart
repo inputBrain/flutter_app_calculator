d8470e1886ccf0f76dfed3d7d4c8039598653f66
Alexander <vsany@meta.ua>
2024-01-17 17:23:19 +0200

    in work. Sign_with_google and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
+
 import 'firebase_options.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
 
@@ -47,7 +50,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   void initState() {
     super.initState();
-    _loadAdsSetting(); 
+    _loadAdsSetting();
     _initBannerAd();
   }
 
@@ -130,7 +133,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
               child: calc,
             ),
           ),
-          if (isAdLoaded && adsOn) 
+          if (isAdLoaded && adsOn)
             Align(
               alignment: Alignment.bottomCenter,
               child: Container(
@@ -139,9 +142,67 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                 child: AdWidget(ad: myBanner),
               ),
             ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                // Call the _signInWithGoogle method to sign in with Google
+                UserCredential? userCredential = await _signInWithGoogle();
+
+                // After successful sign-in, send the Firebase token to your server
+                if (userCredential != null) {
+                  String? firebaseToken = await userCredential.user!.getIdToken();
+                  await _sendFirebaseTokenToServer(firebaseToken!);
+                }
+              },
+              child: Text('Sign in with Google'),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  Future<UserCredential?> _signInWithGoogle() async {
+    try {
+      // Initialize Google Sign-In
+      final GoogleSignInAccount? googleSignInAccount = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleSignInAuthentication = await googleSignInAccount?.authentication;
+
+      // Create GoogleAuthProvider credentials
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication?.accessToken,
+        idToken: googleSignInAuthentication?.idToken,
+      );
+
+      // Sign in with GoogleAuthProvider credentials
+      final UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
+
+      // Return the sign-in result
+      return authResult;
+    } catch (e) {
+      // Handle errors
+      print('Error signing in with Google: $e');
+      return null;
+    }
+  }
+
+  Future<void> _sendFirebaseTokenToServer(String firebaseToken) async {
+    Uri url = Uri.parse('http://localhost:5500/api/Auth/AuthByFirebase?firebaseToken=$firebaseToken');
+
+    try {
+      // Make a POST request to the server
+      var response = await http.post(url);
+
+      // Check the response status
+      if (response.statusCode == 200) {
+        print('Firebase token sent successfully');
+      } else {
+        print('Error sending Firebase token to server. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle any errors here
+      print('Error sending Firebase token to server: $e');
+    }
   }
 }
 
