49dc41b6bc1490e80489b8ae30a4bc6359df1272
Alexander <vsany@meta.ua>
2024-01-23 16:11:32 +0200

    [*] fix sending a post request for Firebase auth with the token to the server

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
@@ -36,19 +38,31 @@ class FirebaseAuthService {
   }
 
   static Future<void> sendFirebaseTokenToServer(String firebaseToken) async {
-    Uri url = Uri.parse('http://localhost:5500/api/Auth/AuthByFirebase?firebaseToken=$firebaseToken');
+    final apiUrl = 'http://10.0.2.2:5500/api/Auth/AuthByFirebase';
 
-    try {
-      var response = await http.post(url);
+    Future<void> sendPostAsync() async {
+      try {
+        var response = await http.post(
+            Uri.parse(apiUrl),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: jsonEncode({
+              "firebaseToken": firebaseToken,
+            }));
 
-      if (response.statusCode == 200) {
-        print('Firebase token sent successfully');
-      } else {
-        print('Error sending Firebase token to server. Status code: ${response.statusCode}');
+        if (response.statusCode == 200) {
+          print('Firebase token sent successfully');
+        } else {
+          print('Error sending Firebase token to server. Status code: ${response.statusCode}');
+        }
+      } catch (e) {
+        print('Error sending Firebase token to server: $e');
       }
-    } catch (e) {
-      print('Error sending Firebase token to server: $e');
     }
+
+    // Call the function to send the request
+    await sendPostAsync();
   }
 
   static Stream<User?> authStateChanges() {
