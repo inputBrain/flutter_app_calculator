acf828069343ff8afc0027a27cdbf8c35bce77d2
Alexander <vsany@meta.ua>
2024-02-09 14:26:53 +0200

    [+] imports.dart

---
+++ lib/imports.dart
@@ -0,0 +1,24 @@
+export 'dart:convert';
+
+export 'firebase_options.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+
+export 'package:flutter/material.dart';
+export 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
+export 'package:purchases_flutter/purchases_flutter.dart';
+export 'package:shared_preferences/shared_preferences.dart';
+
+export 'package:google_mobile_ads/google_mobile_ads.dart';
+export 'package:google_sign_in/google_sign_in.dart';
+
+export 'package:calculator_app/screens/authentication_screen.dart';
+export 'package:calculator_app/screens/calculator_screen.dart';
+export 'package:calculator_app/screens/settings_screen.dart';
+
+export 'package:calculator_app/services/ads_service.dart';
+export 'package:calculator_app/services/firebase_service.dart';
+
+export 'package:calculator_app/models/UserModel.dart';
+
+export 'package:calculator_app/widgets/simple_calculator_widget.dart';
