7dd18681c94ea7c41d8848504e4a70708be24d02
Alexander <vsany@meta.ua>
2024-02-09 14:28:09 +0200

    [+] added package purchases_flutter

---
+++ lib/services/user_manager.dart
@@ -0,0 +1,11 @@
+// import 'package:calculator_app/models/UserModel.dart';
+//
+// class UserManager {
+//   static UserModel? _currentUser;
+//
+//   static get userModel => _currentUser;
+//
+//   static Future<void> setCurrentUser(UserModel? userModel) async {
+//     _currentUser = userModel;
+//   }
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
+import purchases_flutter
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
